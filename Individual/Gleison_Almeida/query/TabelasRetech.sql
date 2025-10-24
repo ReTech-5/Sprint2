d2a7070927479c5668f7dae78e71faae93096d9c
Lucas <wesley@MacBook-Air-de-Lucas.local>
2025-10-24 13:15:24 -0300

    Atualizando repositórios individuais

--- Individual/Gleison_Almeida/query/TabelasRetech.sql
+++ Individual/Gleison_Almeida/query/TabelasRetech.sql
@@ -5,7 +5,7 @@ CREATE DATABASE ReTech;
 USE ReTech;
 
 /*
-Tabela empresa
+Tabela Empresa
 
 - Armazena os dados das empresas que contratam o sistema ReTech
 - Cada empresa tem sensores, usúarios e contatos associados 
@@ -18,7 +18,7 @@ dtFimContrato DATE							-- Data do fim do contrato
 );
 
 /*
-Tabela endereco
+Tabela Endereco
 - Armazena os locais onde os sensores estão instalados
 - No diagrama de solução representa o ponto físico das lixeiras
 */
@@ -26,12 +26,11 @@ CREATE TABLE Endereco(
 idEndereco INT PRIMARY KEY AUTO_INCREMENT, 	-- Identificador único do endereço
 logradouro VARCHAR(45),						-- Rua, avenida ou local
 numero INT,									-- Número do local 
-cep CHAR(9),								-- Código postal (CEP)
-lixeira VARCHAR(45)																																							
+cep CHAR(9),								-- Código postal (CEP)																																						
 );
 
 /*
-Tabela sensor
+Tabela Sensor
 
 - Armazena os sensores instalados nas lixeiras
 - Cada sensor coleta dados de distância (nível do lixo) e envia
@@ -40,12 +39,13 @@ via API para o banco de dados
 CREATE TABLE Sensor(
 idSensor INT PRIMARY KEY AUTO_INCREMENT,	-- identificador do sensor
 codigoSensor VARCHAR(45),					-- Código/Identificador do sensor
+`status` TINYINT,                           -- Status da lixeira (1 = Ativada, 0 = Desativado)
 fkEmpresa INT,								-- Empresa responsável
-	CONSTRAINT fksensorEmpresa
+	CONSTRAINT fkSensorEmpresa
     FOREIGN KEY (fkEmpresa)
     REFERENCES empresa(idEmpresa),
 fkEndereco INT,								-- Local onde o sensor está instalado
-	CONSTRAINT fksensorEndereco 
+	CONSTRAINT fkSensorEndereco 
     FOREIGN KEY (fkEndereco)
     REFERENCES endereco(idEndereco)
 );
@@ -56,19 +56,19 @@ Tabela Contato
 - Representa os contatos da ReTech(clientes)
 */
 CREATE TABLE Contato(
-idContato INT AUTO_INCREMENT UNIQUE NOT NULL, 	-- Identificador único do contato
+idContato INT AUTO_INCREMENT NOT NULL, 	-- Identificador único do contato
 email VARCHAR(45),								-- E-mail do contato
 telefone VARCHAR(12),  							-- Telefone celular
 telefoneFixo VARCHAR(11), 						-- Telefone Fixo
 fkEmpresa INT,           						-- Chave estrangeira
 	CONSTRAINT fkContatoEmpresa					-- Nome da Constrant
     FOREIGN KEY (fkEmpresa)
-    REFERENCES Empresa(idEmpresa),				-- Empresa à qual o contato pertence
-    PRIMARY KEY (idContato, fkEmpresa)			-- Estabelecendo uma entidade fraca
+    REFERENCES empresa(idEmpresa),				-- Empresa à qual o contato pertence
+PRIMARY KEY (idContato, fkEmpresa)		    	-- Estabelecendo uma entidade fraca
 );
 
 /*
-Tabela usúario
+Tabela Usúario
 Representam os operadores das empresas ou o administrador 
 */
 CREATE TABLE Usuario(
