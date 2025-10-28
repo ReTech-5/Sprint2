97d2b327a4ba6412681dff27517927bbbce29033
Lucas <wesley@MacBook-Air-de-Lucas.local>
2025-10-28 09:51:23 -0300

    Corrigindo

---
+++ Grupo/Site_Institucional/js/cadastro.js
@@ -0,0 +1,179 @@
+var listaCaracteresEspeciais = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "¨¨",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "}",
+  "ª",
+  "[",
+  "]",
+  "~",
+  "^",
+  ":",
+  ";",
+  ".",
+  ",",
+  "?",
+  "°",
+  "/",
+];
+var temCaracteresEspeciais = 0;
+var temNumeros = 0;
+
+// Lista de usuários
+var listaUsuarios = [];
+listaUsuarios.push({
+  nome: "Claudio Frizzarini",
+  email: "Claudio@prefeituraSP.com",
+  nivel: "Administrador",
+});
+listaUsuarios.push({
+  nome: "Julia Araripe",
+  email: "Julia@prefeituraCampinas.com",
+  nivel: "Padrão",
+});
+
+// Filtro atual
+var filtro = "todos";
+
+// Div da tabela
+var tabelaDivConteudo = ""; // Variável que vai armazenar o HTML da tabela
+
+// Função para "renderizar" tabela dentro da div
+function mostrarTabela() {
+  tabelaDivConteudo =
+    "<table class='tabela'><tr><th>Nome</th><th>E-mail</th><th>Nível</th></tr>";
+
+  var i = 0;
+  var tamanhoListaUsuario = listaUsuarios.length;
+  while (i < tamanhoListaUsuario) {
+    var usuario = listaUsuarios[i];
+    var mostrar = false;
+
+    if (filtro == "todos") {
+      mostrar = true;
+    } else if (usuario.nivel.toLowerCase() == filtro) {
+      mostrar = true;
+    }
+
+    if (mostrar == true) {
+      tabelaDivConteudo =
+        tabelaDivConteudo +
+        "<tr>" +
+        "<td>" +
+        usuario.nome +
+        "</td>" +
+        "<td>" +
+        usuario.email +
+        "</td>" +
+        "<td>" +
+        usuario.nivel +
+        "</td>" +
+        "</tr>";
+    }
+
+    i++;
+  }
+
+  tabelaDivConteudo = tabelaDivConteudo + "</table>";
+
+  // Atualiza o conteúdo da div
+  tabelaDiv.innerHTML = tabelaDivConteudo;
+}
+
+// Função para cadastrar novo usuário
+function novoUsuario() {
+  var nome = prompt("Digite o nome do novo usuário:");
+  if (nome == "") {
+    alert("Nome obrigatório!");
+  } else {
+    var email = prompt("Digite o e-mail:");
+
+    if (email == "") {
+      alert("E-mail obrigatório!");
+    } else if (!email.includes("@")) {
+      alert("E-mail deve conter @!");
+    } else {
+      var senha = prompt("Digite a senha:");
+
+      while (cont < tamanhoSenha) {
+        if (listaCaracteresEspeciais.includes(senha[cont]) == true) {
+          temCaracteresEspeciais++;
+        }
+        if (!isNaN(senha[cont])) {
+          temNumeros++;
+        }
+        cont++;
+      }
+
+      if (senha == "") {
+        alert("E-mail obrigatório!");
+      } else if (senha) {
+      }
+
+      var nivel = prompt("Informe o nível de acesso (Administrador/Padrão):");
+
+      if (!nivel) {
+        alert("Nível obrigatório!");
+      } else if (nivel != "Administrador" && nivel != "Padrão") {
+        alert("Nível diferente das opções disponíveis: Administrador/Padrão!");
+      } else {
+        usuarios.push({ nome: nome, email: email, nivel: nivel });
+        mostrarTabela();
+      }
+    }
+  }
+}
+
+// Função para editar usuário
+function editarUsuario() {
+  mostrarTabela();
+  var id = prompt("Digite o ID do usuário que deseja editar:");
+  var idNum = id * 1; // converte string em número
+
+  if (idNum < 0 || idNum >= usuarios.length || id == "") {
+    alert("ID inválido!");
+  } else {
+    var u = usuarios[idNum];
+    var novoNome = prompt("Editar nome:", u.nome);
+    var novoEmail = prompt("Editar e-mail:", u.email);
+    var novoNivel = prompt("Editar nível:", u.nivel);
+
+    if (novoNome && novoEmail && novoNivel) {
+      usuarios[idNum].nome = novoNome;
+      usuarios[idNum].email = novoEmail;
+      usuarios[idNum].nivel = novoNivel;
+      mostrarTabela();
+    } else {
+      alert("Todos os campos são obrigatórios!");
+    }
+  }
+}
+
+// Funções de filtro
+function filtrarTodos() {
+  filtro = "todos";
+  mostrarTabela();
+}
+function filtrarAdmin() {
+  filtro = "administrador";
+  mostrarTabela();
+}
+function filtrarPadrao() {
+  filtro = "padrão";
+  mostrarTabela();
+}
+
+// Inicializa
+mostrarTabela();
