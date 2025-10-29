13c6ee078ee6b1e2b7d8c11563a49604ec0f0a14
Lucas <wesley@MacBook-Air-de-Lucas.local>
2025-10-29 18:47:56 -0300

    Atualização Tela de Cadastro

--- Grupo/Site_Institucional/js/cadastro.js
+++ Grupo/Site_Institucional/js/cadastro.js
@@ -32,17 +32,25 @@ var listaCaracteresEspeciais = [
 // Lista de usuários
 var listaUsuarios = [];
 listaUsuarios.push({
-  nome: "Claudio Frizzarini",
-  email: "Claudio@prefeituraSP.com",
-  senha: "Frizza123",
+  nome: "Lucas Quevedo",
+  email: "lucas.castro@sptech.gov",
+  senha: "Polenta7?",
   nivel: "Administrador",
 });
 listaUsuarios.push({
-  nome: "Julia Araripe",
-  email: "Julia@prefeituraCampinas.com",
-  senha: "Julia123",
-  nivel: "Padrão",
+  nome: "Samara Freitas",
+  email: "samara.farias@sptech.gov",
+  senha: "Chocolate9.",
+  nivel: "Administrador",
 });
+
+var nome = "";
+var email = "";
+var senha = "";
+var nivel = "";
+var mensagem = "";
+var campoValidado = false;
+var usuarioValidado = false;
 
 // Filtro atual
 var filtro = "todos";
@@ -50,8 +58,7 @@ var filtro = "todos";
 // Div da tabela
 var tabelaDivConteudo = ""; // Variável que vai armazenar o HTML da tabela
 
-// Card Cadastro Ativo
-var cardCadastro = false
+var mostrarCadastro = false;
 
 // Função para "renderizar" tabela dentro da div
 function mostrarTabela() {
@@ -91,73 +98,43 @@ function mostrarTabela() {
     cont++;
   }
 
-  tabelaDivConteudo = tabelaDivConteudo + "</table>";
+  tabelaDivConteudo += "</table>";
 
   // Atualiza o conteúdo da div
-  tabelaDiv.innerHTML = tabelaDivConteudo;
+  div_tabela.innerHTML = tabelaDivConteudo;
 }
 
 // Função para renderizar o card de cadastro
 function novoUsuario() {
-
-  if(cardCadastro == false){
-    main_container.innerHTML += `
-    <div class="card_cadastro">
-
-      <div class="card_cadastro_header">
-        Cadastro de Usuario
-      </div>
-
-      <div class="card_cadastro_body">
-        <div class="campo_cadastro">
-          <label for="ipt_nome" class="lbl_cadastro">Nome</label>
-          <input placeholder="Insira um nome..." type="text" id="ipt_nome" class="ipt_cadastro">
-        </div>
-
-        <div class="campo_cadastro">
-          <label for="ipt_email" class="lbl_cadastro">E-mail</label>
-          <input placeholder="Insira um e-mail..." type="text" id="ipt_email" class="ipt_cadastro">
-        </div>
-
-        <div class="campo_cadastro">
-          <label for="ipt_senha" class="lbl_cadastro">Senha</label>
-          <input placeholder="Insira uma senha..." type="text" id="ipt_senha" class="ipt_cadastro">
-        </div>
-
-        <div class="campo_cadastro">
-          <label for="slc_nivel" class="lbl_cadastro">Nível de Acesso</label>
-          <select name="" id="slc_nivel" class="ipt_cadastro">
-            <option>Padrão</option>
-            <option>Administrador</option>
-            <option selected disabled value>Selecione um nível</option>
-          </select>
-        </div>
-        <br>
-        <button onclick="cadastrar()" id="btn_cadastrar" class="button">Cadastrar</button>
-        <br>
-        <div id='div_erro'></div>
-      </div>
-    </div>
-    `;
-
-    cardCadastro = true
-  }  
+  if (mostrarCadastro == false) {
+    div_tela_cadastro.style.display = "flex";
+    mostrarCadastro = true;
+  }
+  else if (mostrarCadastro == true) {
+    div_tela_cadastro.style.display = "none";
+    mostrarCadastro = false;
+    ipt_nome.value = "";
+    ipt_email.value = "";
+    ipt_senha.value = "";
+    slc_nivel.value = "#";
+    div_erro.innerHTML = "";
+  }
 }
 
-// Função para cadastrar novo usuário
-function cadastrar() {
-  var nome = ipt_nome.value;
-  var email = ipt_email.value;
-  var senha = ipt_senha.value;
-  var nivel = slc_nivel.value;
+function validarCampos() {
+  nome = ipt_nome.value.trim();
+  email = ipt_email.value.trim().toLowerCase();
+  senha = ipt_senha.value.trim();
+  nivel = slc_nivel.value;
 
   var tamanhoSenha = senha.length;
   var senhaMaiuscula = senha.toUpperCase();
   var senhaMinuscula = senha.toLowerCase();
   var temCaracteresEspeciais = 0;
   var temNumeros = 0;
-  var mensagem = "";
+
   var cont = 0;
+  mensagem = "";
 
   while (cont < tamanhoSenha) {
     if (listaCaracteresEspeciais.includes(senha[cont]) == true) {
@@ -169,7 +146,7 @@ function cadastrar() {
     cont++;
   }
 
-  if (nome == "" || email == "" || senha == "" || nivel == "") {
+  if (nome == "" || email == "" || senha == "" || nivel == "#") {
     mensagem = "Todos os campos devem estar preenchidos antes de prosseguir!";
   } 
   else if (
@@ -183,18 +160,62 @@ function cadastrar() {
     if (!email.includes("@")) {
       mensagem = "- E-mail deve conter @<br>";
     }
+    if (tamanhoSenha < 8) {
+      mensagem += "- Senha deve ter 8 caracteres ou mais  <br>";
+    }
+    if (senha == senhaMaiuscula) {
+      mensagem += "- Senha deve conter letra minúsucla<br>";
+    }
+    if (senha == senhaMinuscula) {
+      mensagem += "- Senha deve conter letra maiúscula<br>";
+    }
     if (temNumeros == 0) {
       mensagem += "- Senha deve conter número<br>";
     }
-  } 
+    if (temCaracteresEspeciais == 0) {
+      mensagem += "- Senha deve conter caractere especial<br>";
+    }
+  }
   else {
-    usuarios.push({ nome: nome, email: email, senha: senha, nivel: nivel });
+    campoValidado = true;
+  }
+}
+
+function usuarioExistente() {
+  var tamanhoListaUsuario = listaUsuarios.length;
+  var cont = 0;
+  var falha = 0;
+
+  while (cont < tamanhoListaUsuario) {
+    var usuario = listaUsuarios[cont];
+
+    if (usuario.email == email) {
+      mensagem += "- Esse e-mail já está sendo utilizado!"
+      falha++;
+    }
+
+    cont++;
+  }
+
+  if (falha == 0) {
+    usuarioValidado = true;
+  }
+}
+
+// Função para cadastrar novo usuário
+function cadastrar() {
+  validarCampos();
+  usuarioExistente();
+
+  if (campoValidado && usuarioValidado) {
+    listaUsuarios.push({ nome: nome, email: email, senha: senha, nivel: nivel });
     mostrarTabela();
   }
 
   div_erro.innerHTML = mensagem;
 }
 
+/*
 // Função para editar usuário
 function editarUsuario() {
   mostrarTabela();
@@ -219,6 +240,7 @@ function editarUsuario() {
     }
   }
 }
+*/
 
 // Funções de filtro
 function filtrarTodos() {
